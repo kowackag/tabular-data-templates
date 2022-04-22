ae7d71ce5ab91e21dd94c9f7c8686a8d54b45dd7
Malgorzata Kowacka <kowackag@gmail.com>
2022-04-22 22:04:09 +0200

    Add searchContent fields

--- src/components/TableBody/TableBody.js
+++ src/components/TableBody/TableBody.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 
 import Row from '../Row/Row';
@@ -6,12 +6,29 @@ import Row from '../Row/Row';
 // import StyledTableBody from './TableBody.styled';
 
 const TableBody = ({ films, colNames }) => {
+
+    const init = {
+        title: '',
+        genre: '',
+        performance: '',
+        year:''
+    }
+
+    const [searchData, setSearchData] = useState(init)
+
+    const changeValue = (e) => {
+        e.preventDefault();
+        setSearchData({...searchData, [e.target.name]: e.target.value})
+    }
+
+    const copyFilms = films.filter(({title, genre, year, performance}) => title.toUpperCase().includes(searchData.title.trim().toUpperCase()) && genre.toUpperCase().includes(searchData.genre.trim().toUpperCase()) && performance.join('').toUpperCase().includes(searchData.performance.trim().toUpperCase()) && year.toString().includes(searchData.year.trim()));
+
     return (
         <tbody>
             <Row>
-                {colNames.map(({ name }) => <td key={name}>{name && <input />}</td>)}
+                {colNames.map(({ name }) => <td key={name}>{name && <input name={name} value={searchData[name]} onChange={changeValue}/>}</td>)}
             </Row>
-            {films.map((film, ind) => {
+            {copyFilms.map((film, ind) => {
                 const { title, genre, performance, year } = film;
                 return (
                     <Row key={ind}>
